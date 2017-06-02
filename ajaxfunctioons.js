de6b02a88b7b22cc063aa9ee0bd998b789231ec3
Caterin A Duarte <cd02048@srcs.loc>
2017-06-02 08:41:26 -0700

    Music

---
+++ ajaxfunctioons.js
@@ -0,0 +1,44 @@
+**
+ * Uses AJAX to query an internet data source for zip codes
+ * @param {string} zipId The element id that has the zip code
+ */
+function findMusic(music) {
+    // First get the zip code from the HTML textbox
+    var yourmusic = document.getElementById(music).value;
+    // Now make a HTTP request
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            // We got a response from the server!
+            if(this.status === 200) {
+                // The request was successful!
+                displayfindMusic(this.responseText);
+            } else if (this.status === 404){
+                // No postal code found
+                displayfindMusic('{ "songs" : "none" }');
+            } else {
+                console.log("We have a problem: " + this.status);
+            }
+        } else {
+            // Waiting for a response...
+        }
+    };
+    // Notice how the URL is appended with the zip code
+    var url = "https://developer.spotify.com/web-api//" + yourmusic;
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+}
+/**
+ * Displays the zip code place given the JSON data
+ * @param {string} data JSON data representing place for given zip code
+ */
+function displayfindMusic(data){
+    var music = JSON.parse(data);
+    if(music.songs === "none") {
+        document.getElementById("music").className = "alert alert-warning";
+        document.getElementById("music").innerHTML = "The songs are" + music;
+    } else {
+        document.getElementById("music").className = "alert alert-success";
+        document.getElementById("music").innerHTML ="there are no songs for this artist"
+    }
+}
